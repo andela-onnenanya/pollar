2a4030f90df2a7aadf62b1ab387bec232dfa8147
Nnenanya Obinna <obinna.nnenanya@andela.com>
2017-10-18 10:08:15 +0100

    supply data to the template

--- pollar/main/views.py
+++ pollar/main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-  return render(request, 'main/home.html')
+  return render(request, 'main/home.html', {'greeting': 'Hi, How is coding going?'})
