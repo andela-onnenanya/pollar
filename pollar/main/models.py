efa6fd5d31aa5f401e7e77148f833efd9ef2eddf
Nnenanya Obinna <obinna.nnenanya@andela.com>
2017-10-19 11:23:57 +0100

    create models

--- pollar/main/models.py
+++ pollar/main/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.contrib import admin
+from django.contrib.auth.models import User
 # Create your models here.
+
+# class List(models.Model):
+#     name = models.CharField(max_length=50)
+#     description = models.CharField(max_length=50)
+#     author = models.ForeignKey(User, related_name='poll_author')
+
+class Poll(models.Model):
+    question = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=255, blank=True)
+    
+    def __str__(self):
+        return self.choice
+
+class Vote(models.Model):
+    poll = models.ForeignKey(Poll)
+    choiceVote = models.ForeignKey(Choice)
+    
+    def __str__(self):
+        return self.choiceVote
