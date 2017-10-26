ac55ed08d13901f6b8fcf4277c23f30e39bf5ea7
Nnenanya Obinna <obinna.nnenanya@andela.com>
2017-10-26 13:24:12 +0100

    enforce unique_together in Choice model

--- pollar/main/models.py
+++ pollar/main/models.py
@@ -11,22 +11,25 @@ from django.contrib.auth.models import User
 class Poll(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=50)
-    author = models.ForeignKey(User, related_name='poll_author')
+    author = models.ForeignKey(User, related_name='poll_author', on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return '%s %s' % (self.title, self.description)
 
 class Choice(models.Model):
-    poll = models.ForeignKey(Poll)
-    choice = models.CharField(max_length=30, blank=True)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    choice = models.TextField()
+
+    class Meta:
+        unique_together = (("choice", "poll"),)
 
     def __str__(self):
         return self.choice
 
 class Vote(models.Model):
     poll = models.ForeignKey(Poll)
-    choiceVote = models.ForeignKey(Choice)
+    choiceVote = models.ForeignKey(Choice, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.choiceVote
