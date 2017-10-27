31957264589f249c044f50abfd277ada55af6056
Nnenanya Obinna <obinna.nnenanya@andela.com>
2017-10-27 17:50:34 +0100

    try fixing wgsi error

--- pollar/pollar/wsgi.py
+++ pollar/pollar/wsgi.py
@@ -17,7 +17,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-os.environ['DJANGO_SETTINGS_MODULE'] = 'pollar.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pollar.settings.settings'
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
