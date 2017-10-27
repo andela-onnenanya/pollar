2d7e0fe985b96cbe81e99b3f4fa4f7fe79fb9472
Nnenanya Obinna <obinna.nnenanya@andela.com>
2017-10-27 16:53:04 +0100

    try fixing wgsi error

--- pollar/wsgi.py
+++ pollar/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os, sys
-sys.path.append(' /Users/andeladeveloper/Projects/Django/pollar')
+sys.path.append(' /Users/andeladeveloper/Projects/Django/pollar/pollar')
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pollar.settings")
