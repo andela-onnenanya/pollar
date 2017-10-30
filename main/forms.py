439b3fda5f0b5b236abe109eb68355acbafc3563
Nnenanya Obinna <obinna.nnenanya@andela.com>
2017-10-30 16:40:45 +0100

    adjust signup form

--- main/forms.py
+++ main/forms.py
@@ -9,6 +9,7 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    password1 = forms.CharField(widget=forms.PasswordInput(), help_text='Required. Use 8 or more characters')
 
     class Meta:
         model = User
