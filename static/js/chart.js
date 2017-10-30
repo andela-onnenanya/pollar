7ac281cbe221ae18a477775bbf65489ab0e0cc57
Nnenanya Obinna <obinna.nnenanya@andela.com>
2017-10-30 09:48:59 +0100

    display vote on UI

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,77 +1,17 @@
 
 //var appUrl = window.location.origin;
 
-(function () {
-  var globalResult;
+$(document).ready(function(){
+  google.charts.load('current', {packages: ['bar', 'corechart', 'table']});
   
   google.charts.setOnLoadCallback(loadChart);
-  google.charts.setOnLoadCallback(updateChart);
   
-  //set the semi-global variables here
-  var pollID = "";
-  var pollClick = "";
-  var globalCurrentPoll = "";
-  var globalData;
-
-  var chartOptions = {
-        'width':650,
-        'chartArea':{width:'70%',height:'70%', left:"25%", top: 50},
-        'backgroundColor' : {fill: '#F2F3F4'},
-        'colors':['#512E5F','#F5B041', '#641E16', '#F39C12', '#C39BD3', '#17202A', '#0B5345'],
-        'height':400};
-
-  //draw chart if there are vote, else display a message
-  function conditionalDraw(counter, data, options){
-    // Instantiate and draw the chart.    
-    var chart = new google.visualization.PieChart(document.getElementById('chart'));
-    if (counter === 0){
-      document.getElementById('chart').innerHTML = "<div rel='no-chart'>This poll has no votes yet. Be the first to vote!</div>"
-    }
-    else{
-      document.getElementById('chart').innerHTML = "";
-    chart.draw(data, options);
-    }
-  }
-
   function loadChart(){
-    var result = globalResult;
-    if(result === "[]" || result === undefined){
-      document.getElementById("poll-list").innerHTML = '<li class="poll-ref">Wao! No Polls here! Please Create One.</li>';
-    }else{
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Name');
-      data.addColumn('number', 'Votes');
-      
-        // Set chart options
-      var options = chartOptions;
-      var voteCounter = 0;
-          
-      while (len){
-        var currentOption = voteOptions[len-1];
-        data.addRow([currentOption.name, currentOption.vote]);
-        optionHtml += '<option class="vote-option" value=' + currentOption._id + '>' + currentOption.name + '</option>';
-        
-        //check for polls with votes other than 0;
-        if(currentOption.vote > 0){ voteCounter++; }
-        len--;
-      }
-      
-      conditionalDraw(voteCounter, data, options);
-    }
-  }
-    
-  //check if user can vote for a poll
-  function voterCheck(){
-    var sessionVOtes = JSON.parse(sessionStorage.getItem("userVotes"));
-    var len = sessionVOtes.length || 0;
-    while (len){
-      var vote = sessionVOtes[len-1];
-      if(vote.pollName === globalCurrentPoll){
-        return false;
-      }
-      len--;
-    }
-    return true;
+    $.ajax({
+      type: "GET",
+      url: `${appUrl}/api${urlPath}`,
+      success: drawChart
+    });
   }
     
   //show link box
@@ -88,76 +28,38 @@
       document.getElementById("linkBox").style.display = "none";
   }
 
-  function updateChart(){
-    return
-    // call endpoint for the submit
-    // from the result, check if vote user can vote again
-    // if yes, draw chart with new result,
-    // else, alert user that he can not vote again
-  }
+});
 
-  function openPoll(){
-    var poll = this.getAttribute("poll");  
-    
-    var pollObj= JSON.parse(poll, reviver);
-    document.getElementsByClassName("poll-ref active")[0].className = "poll-ref";
-    this.className += " active";
-    
-    var voteOptions = pollObj.options,
-        len = voteOptions.length;
-    
-    //reset current pollID
-    pollID = pollObj._id;
-    
-    //update poll options in <select>
-    var optionHtml = '<option value="" disabled selected hidden>Select Whom to vote for...</option>';
+var chartOptions = {
+  'width':650,
+  'chartArea':{width:'70%',height:'70%', left:"25%", top: 50},
+  'backgroundColor' : {fill: '#F2F3F4'},
+  'colors':['#512E5F','#F5B041', '#641E16', '#F39C12', '#C39BD3', '#17202A', '#0B5345'],
+  'height':400};
+
+
+function drawChart(response){
+  const votes = response.votes
+  const message = response.message
+  console.log(votes)
+  if (votes.length == 0){
+    document.getElementById('chart').innerHTML = "<div rel='no-chart'>This poll has no votes yet. Be the first to vote!</div>"
+  }
+  else{
+    var chart = new google.visualization.PieChart(document.getElementById('chart'));
+    document.getElementById('chart').innerHTML = "";
+    var data = new google.visualization.DataTable();    
+    data.addColumn('string', 'Name');
+    data.addColumn('number', 'Votes');
+     
+     // Set chart options
+    var len = votes.length;
+    console.log(len)
     while (len){
-        var currentOption = voteOptions[len-1];      
-        optionHtml += '<option class="vote-option" value=' + currentOption._id + '>' + currentOption.name + '</option>';
-        len--;
-      }
-    
-    var title = pollObj.name.length > 20? pollObj.name.slice(0, 18) + "..." : pollObj.name;
-    document.getElementById("pollViewHead").innerHTML = title + ' :<span> by ' + pollObj.author + '</span>';
-    document.getElementById("pollViewDesc").innerHTML = pollObj.description;
-    document.getElementById("chartTitle").innerHTML = title + ' : Pie Chart.';
-    document.getElementById("pollSelection").innerHTML = optionHtml;
-    //update chart with updateChart function above
-    updateChart(JSON.stringify(pollObj)); 
-  }
-
-  function sendVote(){
-    var value = document.getElementById("pollSelection").value;
-    if(!voterCheck()){
-      document.getElementById("mask").style.display = "block";
-      document.getElementById("displayBox").innerHTML = "Sorry! You have already voted! You can't vote twice!";
-      document.getElementById("displayBox").style.display = "block";
-      setTimeout(function(){
-      document.getElementById("mask").style.display = "none";
-      document.getElementById("displayBox").innerHTML = "Submitting vote . . .";
-      document.getElementById("displayBox").style.display = "none";
-      }, 3000);
-    }else{
-      if (value){
-        //lock the screen first
-        document.getElementById("mask").style.display = "block";
-        document.getElementById("displayBox").style.display = "block";
-        var voteObj = {pollName: globalCurrentPoll},
-            oldSessionVotes = JSON.parse(sessionStorage.getItem("userVotes"));
-        oldSessionVotes.push(voteObj);
-        sessionStorage.setItem("userVotes", JSON.stringify(oldSessionVotes))
-        ajaxFunctions.ajaxRequest('POST', appUrl + "/api/votes?voteid=" + value, updateChart)
-      }
+      var vote = votes[len-1];
+      data.addRow([vote.name, vote.vote]);
+      len--
     }
+    chart.draw(data, chartOptions);
   }
-
-  var submitVote = document.getElementById("submitVote");
-
-  submitVote.addEventListener('click', sendVote, false);
-
-  ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', appUrl + "/api/polls-array", loadPolls));
-
-})();
-
-  
-
+}
--- static/js/polls.js
+++ static/js/polls.js
@@ -1,70 +1,60 @@
-var appUrl = window.location.origin;
-var urlPath = window.location.pathname
 
-function openPoll(e){
-  var id = e.currentTarget.id;
-  window.location = `/polls/${id}`
-}
+  var appUrl = window.location.origin;
+  var urlPath = window.location.pathname
 
-function trimString(word, number){
-  if (word) {
-    var word = word.length > number? word.slice(0, 18) + "..." : word;
-    return word;
-  }else{
-    return 'Chart Title'
+  function openPoll(e){
+    var id = e.currentTarget.id;
+    window.location = `/polls/${id}`
   }
-}
 
-
-
-function addEventToClass (klass){
-  //add click event to each of the polls in the pane
-    for (var i=0; i<klass.length; i++){
-      var pollClass = klass[i];
-      pollClass.addEventListener('click', openPoll, false);
+  function trimString(word, number){
+    if (word) {
+      var word = word.length > number? word.slice(0, 18) + "..." : word;
+      return word;
+    }else{
+      return 'Chart Title'
     }
-}
-
-pollClick = document.getElementsByClassName("poll-ref")
-
-if (pollClick){
-  addEventToClass(pollClick)
-}
-
-function updateChart(data){
-  console.log(data)
-}
-
-function voterCheck(){
-  var choice = document.getElementById("pollSelection").value;
-  if (choice){
-    $.ajax({
-      type: "POST",
-      url: `${appUrl}/api${urlPath}`,
-      data: { choice },
-      success: updateChart
-    });
   }
-}
 
-function sendVote(){
-  var value = document.getElementById("pollSelection").value;
-  var quury = `choice=${value}`
-  if(!voterCheck()){
-    document.getElementById("mask").style.display = "block";
-    document.getElementById("displayBox").innerHTML = "Sorry! You have already voted! You can't vote twice!";
-    document.getElementById("displayBox").style.display = "block";
-    setTimeout(function(){
-    document.getElementById("mask").style.display = "none";
-    document.getElementById("displayBox").innerHTML = "Submitting vote . . .";
-    document.getElementById("displayBox").style.display = "none";
-    }, 3000);
-  }else{
-      if (value){
-        //lock the screen first
-        document.getElementById("mask").style.display = "block";
-        document.getElementById("displayBox").style.display = "block";
-        ajaxFunctions.ajaxRequest('POST', appUrl + "/api/polls/vote?" + value, updateChart)
+  function addActionToClass (klass, action){
+    //add click event to each of the polls in the pane
+      for (var i=0; i<klass.length; i++){
+        var pollClass = klass[i];
+        pollClass.addEventListener('click', action, false);
       }
   }
+
+  pollClick = document.getElementsByClassName("poll-ref")
+
+  if (pollClick){
+    addActionToClass(pollClick, openPoll)
+  }
+
+  function sendVote(){
+    var choice = document.getElementById("pollSelection").value;
+    if (choice){
+      document.getElementById("displayBox").innerHTML = "Submitting vote . . .";
+      document.getElementById("mask").style.display = "block";
+      document.getElementById("displayBox").style.display = "block";
+      $.ajax({
+        type: "POST",
+        url: `${appUrl}/api${urlPath}`,
+        data: { choice },
+        success: function(data){
+          displayMessage(data.message);
+          drawChart(data);
+        }
+      });
+    }
+  }
+
+  
+function displayMessage(message){
+  document.getElementById("mask").style.display = "block";
+  document.getElementById("displayBox").innerHTML = message
+  document.getElementById("displayBox").style.display = "block";
+  setTimeout(function(){
+    document.getElementById("mask").style.display = "none";
+    document.getElementById("displayBox").style.display = "none";
+  }, 3000)
 }
